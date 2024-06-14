61dfbdc9571828e2d9a0d87cca62ca67c4107c43
yang-danny <yang_danny@hotmail.com>
2024-06-14 16:23:50 +1000

    fix upload image error

--- lib/actions/actions.ts
+++ lib/actions/actions.ts
@@ -29,7 +29,12 @@ export const getPublishNews = async () => {
   }
   
   export const getProductDetails = async (productId: string) => {
-    const product = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`
+    const product = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`,
+       {
+        mode: 'no-cors',
+        credentials: 'include',
+        method: 'GET',
+      }
     )
     return await product.json()
   }
